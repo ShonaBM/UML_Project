ceb78af3aaa8897dc8b4e4cb1b7a163d8c083fa5
ShonaBM <shona.briand-monplaisir@efrei.net>
2022-10-26 16:25:41 +0200

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,9 +1,11 @@
+import java.util.function.IntFunction;
+
 public class Student {
     private String lastName;
     private String firstName;
     private String phone;
     private String email;
-    private Integer yearRegistered;
+    private int yearRegistered;
     private String studentNumber;
   
     public double averageGPA(){
