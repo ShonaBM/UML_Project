e81d1cc55c2eb74c85711d53fa1b0d86ff1feb6d
GaboNueve <94602569+GaboNueve@users.noreply.github.com>
2022-10-26 16:14:16 +0200

    Create Subject.java

---
+++ Subject.java
@@ -0,0 +1,13 @@
+public class Subject {
+    private double grade;
+    private String subjectCode;
+    private String subjectDescrpition;
+    
+    
+    public double averageGrade(String subjectCode){
+    
+    }
+    public double averageGradePerSubjectAndDepartment(){
+    
+    }
+}
