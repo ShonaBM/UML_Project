3e395ab460d6f99c51cede7d185d0ddb8a5610ec
ShonaBM <shona.briand-monplaisir@efrei.net>
2022-10-26 15:32:34 +0200

    Create Teacher.java
    classe teacher

---
+++ Teacher.java
@@ -0,0 +1,9 @@
+public class Teacher {
+    
+    private String lastName;
+    private String firstName;
+    private String phone;
+    private String email;
+    private String dateOfRecruitment;
+    private String staffNumber;
+}
